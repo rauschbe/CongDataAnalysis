81af36800fb2c687da87477fbb6ba6fd8550dca6
benni <benjaminstefan.rausch@gmail.com>
2019-05-05 12:22:04 +0200

    setting up first analysis code

---
+++ data_analysis.py
@@ -0,0 +1,21 @@
+def descriptor_einsman(df, name = "no name defined"):
+    col_list = ['ID', 'Einsatz-ID', 'Dauer(min)', 'Gebiet',
+                'Ort Engpass', 'Stufe(%)', 'Ursache', 'Anlagenschluessel', 'Anforderer',
+                'Netzbetreiber', 'Anlagen-ID', 'Entschaedigungspflicht', 'Time (CET)']
+    for i in col_list:
+        if i == 'Time (CET)':
+            print('Starting date', df[i].head(1))
+            print('Ending date', df[i].tail(1))
+        elif i in ['Ursache', 'Stufe(%)', 'Anforderer',
+                   'Netzbetreiber', 'Entschaedigungspflicht']:
+            print('Unique values of {}'.format(i), df[i].unique())
+        elif i == "Dauer(min)":
+            print('Durchschnittliche Dauer', df[i].mean())
+            print('Median Dauer', df[i].median())
+            print('Std Dauer', df[i].std())
+            print('Max Dauer', df[i].max())
+            print('Min Dauer', df[i].min())
+        else:
+            print('Length of {}'.format(i), len(df[i].unique()))
+    return "finished processing of {} \n".format(name)
+
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-from einsmanpreprocessing import read_in_data
+from data_analysis import *
 import pandas as pd
 
-read_in_data(output = True, wdir = '/Users/benni/PycharmProjects/CongDataAnalysis/EinsManDatan')
+bag = pd.read_csv('/Users/benni/Desktop/Uni/Paper/Einsman/bag.csv', low_memory = False)
+edi = pd.read_csv('/Users/benni/Desktop/Uni/Paper/Einsman/edi.csv', low_memory = False)
+shn = pd.read_csv('/Users/benni/Desktop/Uni/Paper/Einsman/shn.csv', low_memory = False)
+ava = pd.read_csv('/Users/benni/Desktop/Uni/Paper/Einsman/ava.csv', low_memory = False)
+
+
+descriptor_einsman(bag, name = 'Bayernwerke AG')
+print('---------------------------------------')
+descriptor_einsman(edi, name = 'EDI.S Netz')
+print('---------------------------------------')
+descriptor_einsman(shn, name = 'Schleswig-Holstein Netz')
+print('---------------------------------------')
+descriptor_einsman(ava, name = 'Avacon Netz')
